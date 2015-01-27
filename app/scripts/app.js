8de366421270d9b0712feef843c32e8e7767131a
Emily Blanchard <emblou2@aol.com>
2015-01-27 12:24:38 -0700

    Made next and previous buttons on player-bar work.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -114,6 +114,10 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 
  blocJams.service('SongPlayer', function() {
+     var trackIndex = function(album, song) {
+       return album.songs.indexOf(song);
+   };
+ 
    return {
      currentSong: null,
      currentAlbum: null,
@@ -124,6 +128,23 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
      },
      pause: function() {
        this.playing = false;
+     },
+       next: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex++;
+       if (currentTrackIndex >= this.currentAlbum.songs.length) {
+         currentTrackIndex = 0;
+       }
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+     },
+      previous: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex--;
+       if (currentTrackIndex < 0) {
+         currentTrackIndex = this.currentAlbum.songs.length - 1;
+       }
+ 
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
      },
      setSong: function(album, song) {
        this.currentAlbum = album;
