17b817b75c518a1b22077d8ed0381e84ea3f93d6
Emily Blanchard <emblou2@aol.com>
2015-02-03 10:48:54 -0700

    Changed slider to update (music location) data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-          { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-          { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-          { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-          { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-          { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+        { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+        { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+        { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+        { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+        { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
     ]
  };
  
@@ -156,6 +156,13 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
           var song = this.currentAlbum.songs[currentTrackIndex];
           this.setSong(this.currentAlbum, song);
      },
+         seek: function(time) {
+          // Checks to make sure that a sound file is playing before seeking.
+          if(currentSoundFile) {
+          // Uses a Buzz method to set the time of the song.
+          currentSoundFile.setTime(time);
+       }
+     },
      setSong: function(album, song) {
           if (currentSoundFile) {
             currentSoundFile.stop();
@@ -185,20 +192,55 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
      return offsetXPercent;
    }
 
+      var numberFromValue = function(value, defaultValue) {
+        if (typeof value === 'number') {
+          return value;
+        }
+ 
+        if(typeof value === 'undefined') {
+          return defaultValue;
+        }
+ 
+        if(typeof value === 'string') {
+          return Number(value);
+        }
+   }
+ 
+
    return {
      templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
      replace: true,
      restrict: 'E',
-     scope: {},
+       scope: {
+          onChange: '&'
+       },
     link: function(scope, element, attributes) {
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
 
+         attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+ 
+
       var percentString = function () {
-         percent = Number(scope.value) / Number(scope.max);
-         return percent + "%";
+          var value = scope.value || 0;
+          var max = scope.max || 100;
+          percent = value / max * 100;
+        return percent + "%";
        }
+
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      };
+
  
        scope.fillStyle = function() {
         console.log("Fillstyle!");
@@ -210,18 +252,18 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
        }
 
       scope.onClickSlider = function(event) {
-        console.log("Onclickslider");
+        
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
-         console.log("Value: " + scope.value);
+            notifyCallback(scope.value);
        }
 
       scope.trackThumb = function() {
-        console.log("Trackthumb clicked");
-         $document.bind('mousemove.thumb', function(event){
+        $document.bind('mousemove.thumb', function(event){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+              notifyCallback(scope.value);
            });
          });
  
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -286,11 +286,11 @@ var setupSeekBars = function() {
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-          { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-          { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-          { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-          { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-          { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+        { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+        { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+        { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+        { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+        { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
     ]
  };
  
@@ -430,6 +430,13 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
           var song = this.currentAlbum.songs[currentTrackIndex];
           this.setSong(this.currentAlbum, song);
      },
+         seek: function(time) {
+          // Checks to make sure that a sound file is playing before seeking.
+          if(currentSoundFile) {
+          // Uses a Buzz method to set the time of the song.
+          currentSoundFile.setTime(time);
+       }
+     },
      setSong: function(album, song) {
           if (currentSoundFile) {
             currentSoundFile.stop();
@@ -447,15 +454,9 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
  });
 
   
-<<<<<<< HEAD
-  blocJams.directive('slider', ['$document', function($document){
-
-     // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
-=======
  blocJams.directive('slider', ['$document', function($document){
 
  // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
->>>>>>> Angular slide drag!
    var calculateSliderPercentFromMouseEvent = function($slider, event) {
      var offsetX =  event.pageX - $slider.offset().left; // Distance from left
      var sliderWidth = $slider.width(); // Width of slider
@@ -465,75 +466,57 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
      return offsetXPercent;
    }
 
-<<<<<<< HEAD
-          scope.trackThumb = function() {
-         $document.bind('mousemove.thumb', function(event){
-           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-           scope.$apply(function(){
-             scope.value = percent * scope.max;
-           });
-         });
+      var numberFromValue = function(value, defaultValue) {
+        if (typeof value === 'number') {
+          return value;
+        }
  
-         //cleanup
-         $document.bind('mouseup.thumb', function(){
-           $document.unbind('mousemove.thumb');
-           $document.unbind('mouseup.thumb');
-         });
-       };
-     }]);
+        if(typeof value === 'undefined') {
+          return defaultValue;
+        }
+ 
+        if(typeof value === 'string') {
+          return Number(value);
+        }
+   }
+ 
 
-  return {
-     templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
-     replace: true,
-     restrict: 'E',
-        scope: {}, // Creates a scope that exists only in this directive.
-    link: function(scope, element, attributes) {
-        // These values represent the progress into the song/volume bar, and its max value.
-        // For now, we're supplying arbitrary initial and max values.
-       scope.value = 0;
-       scope.max = 200;
-    var $seekBar = $(element);
-
-      var percentString = function () {
-         percent = Number(scope.value) / Number(scope.max)  * 100;
-=======
    return {
      templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
      replace: true,
      restrict: 'E',
-     scope: {},
+       scope: {
+          onChange: '&'
+       },
     link: function(scope, element, attributes) {
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
 
+         attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+ 
+
       var percentString = function () {
-         percent = Number(scope.value) / Number(scope.max);
->>>>>>> Angular slide drag!
-         return percent + "%";
+          var value = scope.value || 0;
+          var max = scope.max || 100;
+          percent = value / max * 100;
+        return percent + "%";
        }
+
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      };
+
  
        scope.fillStyle = function() {
-<<<<<<< HEAD
-         return {width: percentString()};
-       }
- 
-       scope.thumbStyle = function() {
-         return {left: percentString()};
-       }
- 
-     
-    }
-  }
-
-       scope.onClickSlider = function(event) {
-        var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-        scope.value = percent * scope.max;
-       }
-
-  
-
-=======
         console.log("Fillstyle!");
          return {width: percentString()};
        }
@@ -543,18 +526,18 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
        }
 
       scope.onClickSlider = function(event) {
-        console.log("Onclickslider");
+        
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
-         console.log("Value: " + scope.value);
+            notifyCallback(scope.value);
        }
 
       scope.trackThumb = function() {
-        console.log("Trackthumb clicked");
-         $document.bind('mousemove.thumb', function(event){
+        $document.bind('mousemove.thumb', function(event){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+              notifyCallback(scope.value);
            });
          });
  
@@ -567,7 +550,6 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
     }
    };
  }]);
->>>>>>> Angular slide drag!
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
